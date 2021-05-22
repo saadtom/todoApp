ec1e371a58f74ca94062d7163264ca5c02664c71
Tommy Tomaye <tommytomaye2018@gmail.com>
2021-05-22 13:26:56 -0700

    Let's create our simple to do list component

---
+++ src/todo-list.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class ToDoList extends Component {
+
+    render() {
+        return (
+            <p>To Do List</p>
+        )
+    }
+}
+
+export default ToDoList;
